684fb2523bdb0bc8ce7d558c8e6450f79668b64d
Kaveh <lcaveh@gmail.com>
2021-12-18 14:24:53 -0800

    Initialize app

---
+++ src/components/app.js
@@ -0,0 +1,14 @@
+import React from "react";
+const App = () => {
+  return (
+    <div>
+      <p className="main">Main</p>
+      <style jsx>{`
+        .main {
+          color: green;
+        }
+      `}</style>
+    </div>
+  )
+}
+export default App;
